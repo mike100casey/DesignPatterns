b5d6f9e62cbdb92fe75bcbef59eb7470d09854fa
michael casey <mike100casey@outlook.ie>
2015-11-18 14:27:39 +0000

    Created observer package, FuelDepo and Observer classes

---
+++ src/observer/FuelDepo.java
@@ -0,0 +1,35 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Michael on 11/18/2015.
+ */
+public class FuelDepo {
+    List<Observer> observers = new ArrayList<Observer>();
+    double state;
+
+    public double getState() {
+        return state;
+    }
+
+    public void setState(double state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer){
+        observers.remove(observer);
+    }
+
+    private void notifyAllObservers() {
+        observers.forEach(observer.Observer::update);
+    }
+
+
+}
+
---
+++ src/observer/Observer.java
@@ -0,0 +1,9 @@
+package observer;
+
+/**
+ * Created by Michael on 11/18/2015.
+ */
+public abstract class Observer {
+    FuelDepo fuelDepo;
+    abstract void update();
+}

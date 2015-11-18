1dba414bd5291dbff9a1292fa989131eebfb21ea
michael casey <mike100casey@outlook.ie>
2015-11-18 13:19:55 +0000

    Created UPS class, updated Fedex cost per kg in fedex class. Created USPS class

---
+++ src/strategy/USPS.java
@@ -0,0 +1,24 @@
+package strategy;
+
+/**
+ * Created by Michael on 11/18/2015.
+ */
+public class USPS implements Shipment {
+    private final double FUELPRICE = 1.70;
+    private final double COSTPERMILE = 0.35;
+    private final double COSTPERKG = 8.50;
+    private final double OVERNIGHTCHARGE = 1.75;
+
+    @Override
+    public double calculate(double miles, double weight, DeliverType deliveryType) {
+        double deliveryPrice = 0;
+        switch (deliveryType){
+            case OVERNIGHT:
+                deliveryPrice = OVERNIGHTCHARGE *(FUELPRICE * ((miles * COSTPERMILE) + (weight * COSTPERKG)));
+                break;
+            case STANDARD:
+                deliveryPrice = OVERNIGHTCHARGE *(FUELPRICE * ((miles * COSTPERMILE) + (weight * COSTPERKG)));
+        }
+        return deliveryPrice;
+    }
+}

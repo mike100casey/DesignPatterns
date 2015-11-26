01ee59138ab674215d4d2affcc2e4456a2941675
michael casey <mike100casey@outlook.ie>
2015-11-26 20:29:07 +0000

    Created word class

---
+++ src/composite/Word.java
@@ -0,0 +1,25 @@
+package composite;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 11/26/2015.
+ */
+public class Word extends LetterComposite {
+
+    public Word(List<Letter> letters) {
+        for(Letter letter: letters){
+            this.add(letter);
+        }
+    }
+
+    @Override
+    protected void printBefore() {
+        System.out.println(" ");
+    }
+
+    @Override
+    protected void printAfter() {
+
+    }
+}

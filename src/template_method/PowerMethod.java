640fc9f633486b0bd4a4d0557bb32bb80a916c75
michael casey <mike100casey@outlook.ie>
2015-11-28 20:05:40 +0000

    Created the power method

---
+++ src/template_method/PowerMethod.java
@@ -0,0 +1,22 @@
+package template_method;
+
+/**
+ * Created by Michael on 11/28/2015.
+ */
+public class PowerMethod extends penaltyRoutine {
+
+    @Override
+    protected String player() {
+        return "Burly Defender";
+    }
+
+    @Override
+    protected void distractGoalie(String player) {
+        System.out.println("The " + player + " stand with his back to the goalie.");
+    }
+
+    @Override
+    protected void strikeBall(String player) {
+        System.out.println("The " + player + " kicks the ball with all their strength down the middle");
+    }
+}

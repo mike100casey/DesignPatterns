606ddd86b5146ded8844b26c0308ae2330ed12dd
michael casey <mike100casey@outlook.ie>
2015-11-28 20:11:29 +0000

    Created the placement method

---
+++ src/template_method/PlacementMethod.java
@@ -0,0 +1,22 @@
+package template_method;
+
+/**
+ * Created by Michael on 11/28/2015.
+ */
+public class PlacementMethod extends penaltyRoutine {
+
+    @Override
+    protected String player() {
+        return "Stricker";
+    }
+
+    @Override
+    protected void distractGoalie(String player) {
+        System.out.println("The " + player + " does a shimmy as he approaches the ball.");
+    }
+
+    @Override
+    protected void strikeBall(String player) {
+        System.out.println("The " + player + " kicks the ball into the top corner");
+    }
+}

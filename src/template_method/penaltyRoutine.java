c08de5b971668040d2b25186f016d56a65331fbe
mike100casey <mike100casey@outlook.ie>
2015-11-28 20:10:55 +0000

    Update penaltyRoutine.java

--- src/template_method/penaltyRoutine.java
+++ src/template_method/penaltyRoutine.java
@@ -3,7 +3,7 @@ package template_method;
 /**
  * Created by Michael on 11/28/2015.
  */
-public abstract class PenaltyRoutine {
+public abstract class penaltyRoutine {
 
     protected abstract String player();
     protected abstract void distractGoalie(String player);

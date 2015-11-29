694a97a50c660f3c9564d40d69e8fcd827694a1b
michael casey <mike100casey@outlook.ie>
2015-11-29 15:26:15 +0000

    Created volkswagen, vw engine class

---
+++ src/factory_method/VW_Engine.java
@@ -0,0 +1,23 @@
+package factory_method;
+
+/**
+ * Created by Michael on 11/29/2015.
+ */
+public class VW_Engine implements Engine {
+
+    private EngineType engineType;
+
+    public VW_Engine(EngineType engineType) {
+        this.engineType = engineType;
+    }
+
+    @Override
+    public EngineType getEngineType() {
+        return engineType;
+    }
+
+    @Override
+    public String toString(){
+        return "VW " + engineType;
+    }
+}
---
+++ src/factory_method/Volkswagen.java
@@ -0,0 +1,12 @@
+package factory_method;
+
+/**
+ * Created by Michael on 11/29/2015.
+ */
+public class Volkswagen implements EngineManufacturer {
+
+    @Override
+    public Engine buildEngine(EngineType engineType) {
+        return new VW_Engine(engineType);
+    }
+}

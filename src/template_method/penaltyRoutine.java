5fa53c5bb4b8f8809df5c8dacff45f1a2c7a5c51
michael casey <mike100casey@outlook.ie>
2015-12-08 19:45:35 +0000

    created non abstract method place ball in the penalty routine class

--- src/template_method/penaltyRoutine.java
+++ src/template_method/penaltyRoutine.java
@@ -8,10 +8,14 @@ public abstract class penaltyRoutine {
     protected abstract String taker();
     protected abstract void distractGoalie(String player);
     protected abstract void strikeBall(String player);
+    protected String placeBall(){
+        return "The player places the ball on the penalty spot";
+    }
 
     public void takePenalty(){
         String player = taker();
         System.out.println("The " + player + " is taking the penalty.");
+        placeBall();
         distractGoalie(player);
         strikeBall(player);
     }
